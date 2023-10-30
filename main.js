0b87088be3fb1f1150c2a9ece3dfc62b45832b09
julia090710 <148766007+julia090710@users.noreply.github.com>
2023-10-30 08:59:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function tocaSomPom () {
+    document.querySelector('.tecla_pom'); 
+}
+
+document.querySelector('#som_tecla_pom').play().onclick = tocaSomPom;
+
+
+function tocaSomClap () {
+    document.querySelector('.tecla_pom'); 
+}
+
+document.querySelector('#som_tecla_clap').play().onclick = tocaSomPom;
