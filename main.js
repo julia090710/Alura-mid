8b819dace1bafc26a2cc19de73e93c5324c3e414
julia090710 <148766007+julia090710@users.noreply.github.com>
2023-11-06 09:07:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+// Pom----------------------------------------
 function tocaSomPom () {
     document.querySelector('.tecla_pom'); 
 }
 
-document.querySelector('#som_tecla_pom').play().onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+listaDeTeclas[0].onclick = tocaSomPom
 
-function tocaSomClap () {
-    document.querySelector('.tecla_pom'); 
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('#som_tecla_clap').play().onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
